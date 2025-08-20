6f91fca78b00f0a698f710794952a99ac7c73fec
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2025-08-20 15:04:37 +0530

    Multiplication

---
+++ Multiplication6to9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Multiplication6to9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] result = new int[4]; // for 6,7,8,9
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
