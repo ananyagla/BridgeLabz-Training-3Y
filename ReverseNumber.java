3540f2a43c2c72bf35af8fd3bd85b07979bc5183
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2025-08-20 15:47:03 +0530

    Reverse A Number

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long num = sc.nextLong();
+
+        String numStr = Long.toString(num);
+        int[] digits = new int[numStr.length()];
+
+        for (int i = 0; i < numStr.length(); i++) {
+            digits[i] = numStr.charAt(i) - '0';
+        }
+
+        System.out.print("Reversed number: ");
+        for (int i = digits.length - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+    }
+}
+
