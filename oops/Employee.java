1967b969c15f3a1fdc0530285c6bc0c4f8e6e789
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2025-09-04 14:53:04 +0530

    OOPS Problems

---
+++ oops/Employee.java
@@ -0,0 +1,25 @@
+package oops; 
+
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Ananya", 101, 50000);
+        emp.displayDetails();
+    }
+}
+
